e2d9958fd570b09dbd87d8c73b21e35dc8d40a83
atoolate <alexdelaet00@gmail.com>
2025-07-06 23:21:32 +0200

    feat: Update font styles and enhance hero section content

--- typing.js
+++ typing.js
@@ -1,7 +1,7 @@
 
   document.addEventListener("DOMContentLoaded", function () {
     new Typed("#typed", {
-      strings: ["Designer", "Developer"],
+      strings: ["Creative Thinker", "Problem Solver", "UX/UI Designer", "Lookig for a job"],
       typeSpeed: 150,  // Speed for typing
       backSpeed:150,   // Speed for deleting
       backDelay: 2000, // Delay before starting to delete
